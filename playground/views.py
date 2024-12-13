79e4466a44ce0da0767685cd1a928ae4480e726f
eugene_zaharch <eugenezaharchenko@gmail.com>
2024-12-13 09:34:41 +0200

    select_related queries; lots of static files

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.db.models import Q, F
-from store.models import Product, OrderItem
+from store.models import Product, OrderItem, Order
 
 
 # Create your views here.
 def hello(request):
+    queryset = Product.objects.only('id', 'title')
+    # queryset = Product.objects.defer('description') - 'description' will be excluded
+    print(queryset)
     return render(request, 'hello.html', {'name': 'eugene'})
     # return HttpResponse('<h2>Hello<h2>')
 
@@ -20,13 +23,18 @@ def get_products(request):
     # queryset = Product.objects.filter(Q(inventory__lte=14) | Q(price__in=(6500, 6200)))
     # queryset = Product.objects.filter(inventory=F('price'))
     # queryset = Product.objects.order_by('price', '-description').reverse()[:5]
-    queryset = Product.objects.values('id', 'title', 'collection__title')
+    # queryset = Product.objects.values('id', 'title', 'collection__title')
+    # queryset = Product.objects.select_related('collection').all()  #  INNER JOIN "collection"
+    queryset = Product.objects.prefetch_related('promotions').select_related('collection').all()  #  INNER JOIN, use 'prefetch' when have many-to-many relationship
+    # queryset = Product.objects.filter(id__exact=1).first()
     # product = Product.objects.order_by('price')[0]
     # product = Product.objects.earliest('price')
     # product = Product.objects.latest('inventory')
+    orders = Order.objects.select_related('customer').order_by('-placed_at')[:3]
     if queryset:
         return render(request, 'hello.html', {'name': 'Eugene',
-                                              'products': list(queryset)})
+                                              'products': list(queryset),
+                                              'orders': list(orders)})
     # return render(request, 'hello.html', {'name': 'Eugene',
     #                                       'product': product})
 
