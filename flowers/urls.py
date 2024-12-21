78286d1844289339b62089ed1411ff6cc27af324
Eugene Zakharchenko <eugenezaharchenko@gmail.com>
2024-12-21 21:52:23 +0200

    forms added

--- flowers/urls.py
+++ flowers/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('create-flower/', views.create_flower, name='create_flower'),
     path('create-client/', views.create_client, name='create_client'),
-    path('get_flower/', views.get_flower, name='get_flower')
+    path('get_flower/', views.get_flower, name='get_flower'),
+    path('try_form/', views.my_form, name='my_form'),
+    path('try_form_class/', views.MyFormView.as_view(), name='my_form'),
+    path('try_form_model/', views.ModelFormView.as_view(), name='my_form'),
 ]
