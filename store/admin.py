d5d8d631bf874eb63215d627645d59644e028491
Eugene Zakharchenko <eugenezaharchenko@gmail.com>
2025-05-20 14:37:49 +0300

    CollectionAdmin: queryset overridden, links to products added

--- store/admin.py
+++ store/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
+from django.http import HttpRequest
+from django.db.models import QuerySet, Count
+from django.urls import reverse
+from django.utils.html import format_html, urlencode
 from . import models
 
 
@@ -31,6 +35,21 @@ class OrderAdmin(admin.ModelAdmin):
     list_display = ('id', 'placed_at', 'customer')
 
 
-admin.site.register(models.Collection)
+@admin.register(models.Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ('title', 'products_count')
+
+    @admin.display(ordering='products_count')
+    def products_count(self, collection):
+        url = reverse("admin:store_product_changelist") + '?' + urlencode({
+            'collection__id': str(collection.id)
+        })
+        return format_html('<a href="{}">{}</a>', url, collection.products_count)
+
+    def get_queryset(self, request: HttpRequest) -> QuerySet:
+        return super().get_queryset(request).annotate(
+            products_count=Count('product')
+        )
+
 admin.site.register(models.Customer, CustomerAdmin)
 # admin.site.register(models.Product, ProductAdmin) use this or @admin.register
